d7218d339a89396b8ceaa08572abe96b4b28ff74
cookiefactory@gmail.com <cookiefactory@gmail.com>
2015-11-13 21:52:20 -0500

    initial commit

---
+++ src/main/java/com/cf/helloworld/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cf.helloworld;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author David
+ */
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        final Logger logger = LoggerFactory.getLogger(Main.class);
+
+        logger.info("Hello World!");
+    }
+}
